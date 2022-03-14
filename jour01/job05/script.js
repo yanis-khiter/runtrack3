53cd0e0012d02e523b8e626633d1454610de9456
yanis-khiter <yanis.khiter@laplateforme.io>
2022-03-14 13:52:02 +0100

    job05 ok

---
+++ jour01/job05/script.js
@@ -0,0 +1,17 @@
+
+function afficherjourssemaine() {
+}
+
+var jourssemaines=new Array();
+
+jourssemaines[0]="Lundi";
+jourssemaines[1]="Mardi";
+jourssemaines[2]="Mercredi";
+jourssemaines[3]="Jeudi";
+jourssemaines[4]="Vendredi";
+jourssemaines[5]="Samedi";
+jourssemaines[6]="Dimanche";
+
+var longueur=jourssemaines.length;
+
+console.table(jourssemaines);
